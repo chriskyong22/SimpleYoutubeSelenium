b6b21b21453c8c852d78f7ed9c44e72fe5c07e20
chriskyong22 <55605854+chriskyong22@users.noreply.github.com>
2021-05-27 19:30:14 -0400

    Added spotify retrieval based off url

--- soupifytest.py
+++ soupifytest.py
@@ -1,39 +1,55 @@
 import urllib;
 import urllib.parse;
 import urllib.request;
-from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait 
 from selenium.webdriver.common.by import By 
 from dotenv import load_dotenv
-load_dotenv()
 import os
+import spotipy
+from spotipy import client
+from spotipy.oauth2 import SpotifyClientCredentials
+import youtube_dl 
+
+load_dotenv()
 
 # yt-simple-endpoint style-scope ytd-video-renderer
-def searchYoutube(songName):
+def searchYoutube(songs):
 
     driver = webdriver.Firefox(executable_path=os.environ.get("geckodriverPath"))
-    
-    query = urllib.parse.quote(songName)
-    url = "https://www.youtube.com/results?search_query=" + query
-    driver.get(url)
-
     VIDEO_ELEMENT = "video-title"
-
-    elements = driver.find_elements_by_id(VIDEO_ELEMENT)
-    links = []
-
-    for element in elements:
-        links.append(element.get_attribute("href"))
-        print(links[-1])
-
+    songLinks = []
+    processed = 0
+    for song in songs:
+        query = urllib.parse.quote(song)
+        url = "https://www.youtube.com/results?search_query=" + query
+        driver.get(url)
+        elements = driver.find_elements_by_id(VIDEO_ELEMENT)
+        links = []
+        for element in elements:
+            if (element.get_attribute("href") == None):
+                continue
+            links.append(element.get_attribute("href"))
+        songLinks.append(links)
+        processed += 1
+        print (f"Progress: {(processed / len(songs)) * 100}")
     driver.close()
-
-    return links
+    return songLinks
     
+def getSpotifySongs(playlistID):
+    songs = []
+    spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials(
+        client_id=os.environ.get("spotify-client-id"), 
+        client_secret=os.environ.get("spotify-client-secret")))
+    results = spotify.playlist(playlistID, fields="tracks")
+    for item in results['tracks']['items']:
+        songs.append(f"{item['track']['name']} {item['track']['artists'][0]['name']}")
+    return songs
 
 if (__name__ == "__main__"):
-    youtubeLinks = searchYoutube("tester")
-    print(youtubeLinks)
+    playlistID = '37i9dQZF1DX7Jl5KP2eZaS'
+    urls = []
+    urls = searchYoutube(getSpotifySongs(playlistID))
+    print(urls)
